eb7e830be94ec7b4c79013e8787361ad5b544343
Keshav Santhanam <keshavsanthanam@wirelessprv-10-192-84-17.near.illinois.edu>
2017-03-28 17:32:00 -0500

    Removed ops store and incomplete ops

--- src/ops/operation.h
+++ src/ops/operation.h
@@ -1,14 +1,11 @@
 #ifndef OPS_OPERATION_H_
 #define OPS_OPERATION_H_
 
-// #include "add_bias.h"
 #include "apply_weight.h"
 #include "err.h"
 #include "read_input.h"
 #include "sigmoid.h"
 #include "../graph/node.h"
-
-// #include "softmax.h"
 
 #include <Eigen/Dense>
 
@@ -17,16 +14,12 @@ using Eigen::MatrixXd;
 class Operation {
  public:
   static void compute(Node* n, bool fwd) {
-    if (n->op() == "bias") {
-      // AddBias::compute(n, fwd);//input, weight, output);
-    } else if (n->op() == "error") {
+    if (n->op() == "error") {
       Error::compute(n, fwd);
     } else if (n->op() == "input") {
       ReadInput::compute(n, fwd);
     } else if (n->op() == "sigmoid") {
       Sigmoid::compute(n, fwd);
-    } else if (n->op() == "softmax") {
-      //Softmax::compute(input, weight, output);
     } else if (n->op() == "weight") {
       ApplyWeight::compute(n, fwd);
     }
